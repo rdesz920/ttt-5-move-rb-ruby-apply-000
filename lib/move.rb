f0692eb4216d103c7c63d4f2d45da01078e16cdd
Rob Desz <rob.desz@gmail.com>
2020-10-10 18:35:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,3 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def input_to_index(user_input)
+  converted_input = user_input.to_i
+  converted_input -= 1
+  return converted_input
+end
+
